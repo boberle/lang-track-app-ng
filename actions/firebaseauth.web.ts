7fde193bc1e4db8f4eada43c8160b94e9341a8cc
Bruno Oberle <bruno@boberle.com>
2025-01-11 01:39:36 +0100

    [revive] auth actions: use emulator for web rendering

--- actions/firebaseauth.web.ts
+++ actions/firebaseauth.web.ts
@@ -1,8 +1,19 @@
-import { getAuth } from 'firebase/auth';
+import {getAuth} from 'firebase/auth';
 import getFirebaseApp from "@/actions/firebaseapp";
+import {Auth, connectAuthEmulator} from "@firebase/auth";
+import appConfig from "@/const/lta";
+
+
+let _auth: Auth | null = null;
 
 const getFirebaseAuth = () => {
-    return getAuth(getFirebaseApp());
+    if (!_auth) {
+        _auth = getAuth(getFirebaseApp());
+        if (appConfig.authEmulatorURL != null) {
+            connectAuthEmulator(_auth, appConfig.authEmulatorURL);
+        }
+    }
+    return _auth;
 }
 
 export default getFirebaseAuth;
