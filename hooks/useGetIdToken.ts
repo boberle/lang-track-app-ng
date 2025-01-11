9e4efde34aa9b60f0e5598c42836c5f79293d1f3
Bruno Oberle <bruno@boberle.com>
2025-01-11 10:23:43 +0100

    [revive] hooks: introduce useGetIdToken

---
+++ hooks/useGetIdToken.ts
@@ -0,0 +1,22 @@
+import AuthContext from "@/store/auth";
+import { useCallback, useContext } from "react";
+import { useRouter } from "expo-router";
+
+const useGetIdToken = () => {
+  const ctx = useContext(AuthContext);
+  const router = useRouter();
+
+  const getIdToken = useCallback(async () => {
+    if (ctx.user === null) {
+      router.replace("/login");
+      throw new Error("User not authenticated");
+    }
+    return await ctx.user.getIdToken();
+  }, []);
+
+  return {
+    getIdToken,
+  };
+};
+
+export default useGetIdToken;
