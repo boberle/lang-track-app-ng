dea6ddd0c2545e0f649d8c24e3fe7c0bdc529de6
Bruno Oberle <bruno@boberle.com>
2025-01-11 11:16:59 +0100

    [revive] hook: introduce a fetch_item hook

---
+++ hooks/_fetch_item.ts
@@ -0,0 +1,51 @@
+import { useCallback, useEffect, useState } from "react";
+import useFetch, { FetchDataOptions } from "./_fetch";
+
+const useFetchItem = <T>(converter?: (o: any) => T) => {
+  const {
+    data,
+    isLoading,
+    isError: isFetchError,
+    fetchData,
+    statusCode,
+  } = useFetch();
+  const [isError, setIsError] = useState<boolean>(false);
+  const [item, setItem] = useState<T | null>(null);
+
+  const fetchItem = useCallback(
+    async (url: URL, options: FetchDataOptions = {}) => {
+      setIsError(false);
+      fetchData(url, options);
+    },
+    [fetchData],
+  );
+
+  useEffect(() => {
+    if (data === null) {
+      setItem(null);
+      return;
+    }
+    if (converter == null) return;
+    try {
+      const convertedData = converter(data);
+      setItem(convertedData);
+    } catch (error) {
+      console.log(error);
+      setIsError(true);
+    }
+  }, [converter, data]);
+
+  useEffect(() => {
+    setIsError((prev) => prev || isFetchError);
+  }, [isFetchError]);
+
+  return {
+    item,
+    fetchItem,
+    isLoading,
+    isError,
+    statusCode,
+  };
+};
+
+export default useFetchItem;
