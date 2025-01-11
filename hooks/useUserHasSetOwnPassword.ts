adabd457b515c1f845bf2fa9eebc271f0571b46a
Bruno Oberle <bruno@boberle.com>
2025-01-11 01:47:05 +0100

    [revive] useUserHasSetOwnPassword hook: remove isLoading state

--- hooks/useUserHasSetOwnPassword.ts
+++ hooks/useUserHasSetOwnPassword.ts
@@ -5,18 +5,14 @@ const useUserHasSetOwnPassword = () => {
   const [userHasSetOwnPassword, setUserHasSetOwnPassword] = useState<
     boolean | null
   >(null);
-  const [isLoading, setIsLoading] = useState(false);
 
   const checkUserHasSetOwnPassword = useCallback(async (user: User) => {
-    setIsLoading(true);
     const idTokenResult = await user.getIdTokenResult();
     setUserHasSetOwnPassword(idTokenResult.claims.hasSetOwnPassword === true);
-    setIsLoading(false);
   }, []);
 
   return {
     userHasSetOwnPassword,
-    isLoading,
     checkUserHasSetOwnPassword,
   };
 };
