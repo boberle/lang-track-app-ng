0e51ad1cd3ed531d32b743c67c2981c9304bb6a4
Bruno Oberle <bruno@boberle.com>
2025-01-11 01:36:47 +0100

    [revive] auth layout: use auth context

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,88 +1,13 @@
-import { Redirect, Stack } from "expo-router";
-import useAuth from "@/hooks/useAuth";
-import CommonLoadingComponent from "@/components/common/CommonLoadingComponent";
-import usePushNotifications from "@/hooks/usePushNotifications";
-import { useEffect, useState } from "react";
-import useRegisterDevice from "@/hooks/fetch_register";
-import CommonErrorComponent from "@/components/common/CommonErrorComponent";
-import useUserHasSetOwnPassword from "@/hooks/useUserHasSetOwnPassword";
+import { Stack } from "expo-router";
+import { AuthContextProvider } from "@/store/auth";
 
 export default function AppLayout() {
-  const [key, setKey] = useState<number>(0);
-  const { user, isLoading: isUserLoading } = useAuth();
-  const {
-    isLoading: isHasSetOwnPasswordLoading,
-    userHasSetOwnPassword,
-    checkUserHasSetOwnPassword,
-  } = useUserHasSetOwnPassword();
-  const {
-    registerDevice,
-    isLoading: isRegisterDeviceLoading,
-    isError: isRegisterDeviceError,
-    isSuccess: isRegisterDeviceSuccess,
-  } = useRegisterDevice();
-
-  const { expoPushToken, registerForPushNotifications } =
-    usePushNotifications();
-
-  useEffect(() => {
-    const f = async () => {
-      if (!user) return;
-      if (!userHasSetOwnPassword) return;
-      if (!expoPushToken?.data) return;
-
-      const token = await user.getIdToken();
-      await registerDevice(expoPushToken.data, token);
-    };
-    f();
-  }, [user, expoPushToken, key, userHasSetOwnPassword]);
-
-  useEffect(() => {
-    if (user) {
-      checkUserHasSetOwnPassword(user);
-    }
-  }, [user, checkUserHasSetOwnPassword, key]);
-
-  useEffect(() => {
-    if (user && userHasSetOwnPassword) {
-      registerForPushNotifications();
-    }
-  }, [user, userHasSetOwnPassword, key]);
-
-  if (isUserLoading || isHasSetOwnPasswordLoading || isRegisterDeviceLoading) {
-    return <CommonLoadingComponent />;
-  }
-
-  if (user == null) {
-    return <Redirect href={"/login"} />;
-  }
-
-  if (userHasSetOwnPassword === false) {
-    return <Redirect href={"/change-password"} />;
-  }
-
-  if (isRegisterDeviceLoading) {
-    return <CommonLoadingComponent />;
-  }
-
-  if (!expoPushToken) {
-    return <CommonLoadingComponent />;
-  }
-
-  if (isRegisterDeviceError || !isRegisterDeviceSuccess) {
-    return (
-      <CommonErrorComponent
-        message="Failed to register for push notifications."
-        offerToLogout={true}
-        onRetry={() => setKey((p) => p + 1)}
-      />
-    );
-  }
-
   return (
-    <Stack>
-      <Stack.Screen name="(main)" options={{ headerShown: false }} />
-      <Stack.Screen name="assignments" options={{ headerShown: false }} />
-    </Stack>
+    <AuthContextProvider>
+      <Stack>
+        <Stack.Screen name="(main)" options={{ headerShown: false }} />
+        <Stack.Screen name="assignments" options={{ headerShown: false }} />
+      </Stack>
+    </AuthContextProvider>
   );
 }
