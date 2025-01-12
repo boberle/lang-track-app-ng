4b0f3d4bb6d476a6b5bad9dbd86f0344b5783b49
Bruno Oberle <bruno@boberle.com>
2025-01-12 19:18:29 +0100

    [revive] assignment to complete button: push assignment pathname instead of replace

--- components/home/AssignmentToCompleteButton.tsx
+++ components/home/AssignmentToCompleteButton.tsx
@@ -33,7 +33,7 @@ const AssignmentToCompleteButton = ({
   }, []);
 
   const handlePress = () => {
-    router.replace({
+    router.push({
       pathname: "/assignments/[id]",
       params: { id: assignmentToComplete.id },
     });
