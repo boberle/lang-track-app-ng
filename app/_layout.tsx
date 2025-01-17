28ef6bdb63b668569d31eb73fdd5d16331bb3736
Bruno Oberle <bruno@boberle.com>
2025-01-17 23:26:03 +0100

    background: fix white status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import { Slot } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
       <Slot />
     </SafeAreaProvider>
   );
