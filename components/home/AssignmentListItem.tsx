5b6cbd27fa0674341079a6614d8fc07041286834
Bruno Oberle <bruno@boberle.com>
2025-01-23 09:49:10 +0100

    home (assignment list): don't show hour of assignments, only dates

--- components/home/AssignmentListItem.tsx
+++ components/home/AssignmentListItem.tsx
@@ -15,9 +15,6 @@ const AssignmentListItem = ({ assignment }: AssignmentListItemProps) => {
             year: "numeric",
             month: "2-digit",
             day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
           })}
         </Text>
       </View>
